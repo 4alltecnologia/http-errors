621910bf3bfa2e93bd7cb37a3bc40fe0d39955ce
Ariel Ril <arielril@gmail.com>
2019-05-01 21:23:26 -0300

    :art: modify the exports of the errors and the pkg name

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
+const R = require('ramda');
 const errors = require('./errors');
 const CustomError = require('./CustomError');
 
-module.exports = {
-  CustomError,
-  ...errors,
-};
+module.exports = R.mergeDeepRight({ CustomError }, errors);
